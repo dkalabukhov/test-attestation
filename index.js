e66a93880a912f1a7f79e0969c8fec312f9da2bb
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-04-26 14:47:36 +0300

    Step 1, step 2, step 3 done

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-import _ from 'lodash'
+import _ from 'lodash';
 
-export default function solution(content){
+export default function solution(content) {
   // BEGIN
+  const rows = content.split('\n');
+  const data = rows.slice(1, rows.length - 1);
+  console.log(`Count: ${data.length}`);
+
+  const cities = data.map((row) => row.split(',')[7]);
+  const uniques = [];
+  for (let city of cities) {
+    if (!uniques.includes(city)) {
+      uniques.push(city);
+    }
+  }
+  console.log(`Cities: ${uniques.sort().join(', ')}`);
+
+  const humidities = data.map((row) => row.split(',')[3]);
+  const minHumidity = humidities.reduce((acc, elem) => {
+    if (elem < acc) return elem;
+    else return acc;
+  });
+  const maxHumidity = humidities.reduce((acc, elem) => {
+    if (elem > acc) return elem;
+    else return acc;
+  });
+
+  console.log(`Humidity: Min: ${minHumidity}, Max: ${maxHumidity}`);
 
   // END
-
 }
