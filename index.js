2ba9ac8d04244a0e1a7f26e3d38f00dd202b0ee6
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-04-26 19:43:57 +0300

    Step 5 done

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ export default function solution(content) {
   // BEGIN
   const rows = content.split('\n');
   const data = rows.slice(1, rows.length - 1);
+
+  // 1 step
   console.log(`Count: ${data.length}`);
 
+  // 2 step
   const cities = data.map((row) => row.split(',')[7]);
   const uniques = [];
   for (let city of cities) {
@@ -15,6 +18,7 @@ export default function solution(content) {
   }
   console.log(`Cities: ${uniques.sort().join(', ')}`);
 
+  // 3 step
   const humidities = data.map((row) => row.split(',')[3]);
   const minHumidity = humidities.reduce((acc, elem) => {
     if (elem < acc) return elem;
@@ -27,8 +31,18 @@ export default function solution(content) {
 
   console.log(`Humidity: Min: ${minHumidity}, Max: ${maxHumidity}`);
 
+  // 4 step
   const collection = data.map((row) => row.split(','));
   const hottestTemp = _.max(collection, (row) => row[1]);
   console.log(`HottestDay: ${hottestTemp[0]} ${hottestTemp[7]}`);
+
+  // 5 step
+  const obj = _.groupBy(collection, (row) => row[7]);
+  const res = _.map(obj, (rows, city) => {
+    const meanTemperature = _.mean(rows.map((el) => Number(el[1])));
+    return [meanTemperature, city];
+  });
+  const hottestCity = _.max(res, (row) => row[0]);
+  console.log(`HottestCity: ${hottestCity[1]}`);
   // END
 }
